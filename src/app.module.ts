7a6f62cb5622d2e3bc284e0c5add80fd6b3e00e7
Uwancha <sito2alemu@gmail.com>
2024-06-29 15:05:53 +0300

    feat: Configure Mongoose with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { FeedbackModule } from './feedback/feedback.module';
 import { TicketModule } from './ticket/ticket.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, FeedbackModule, TicketModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    FeedbackModule,
+    TicketModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
