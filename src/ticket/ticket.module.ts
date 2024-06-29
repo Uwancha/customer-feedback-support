59375001b78b1cd28e2e3f6c7eed8b9adf97aa52
Uwancha <sito2alemu@gmail.com>
2024-06-30 01:08:40 +0300

    feat: Set up module for ticket

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ticket, TicketSchema } from './schemas/ticket.schema';
+import { TicketRepository } from './ticket.repository';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Ticket.name, schema: TicketSchema}])],
   controllers: [TicketController],
-  providers: [TicketService]
+  providers: [TicketService, TicketRepository]
 })
 export class TicketModule {}
