3fd7f92f62d822ae998b917f29c9a26fae53b6cc
Uwancha <sito2alemu@gmail.com>
2024-06-29 14:40:32 +0300

    chore: Set up  project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, FeedbackModule, TicketModule],
   controllers: [AppController],
   providers: [AppService],
 })
