10fc2a14d040bf152dedc266bfeae021b83b3f26
Uwancha <sito2alemu@gmail.com>
2024-06-30 01:08:04 +0300

    feat: Set up controller for ticket

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { TicketService } from './ticket.service';
+import { TicketDocument } from './schemas/ticket.schema';
+import { CreateTicketDto } from './dto/create-ticket-dto';
 
-@Controller('ticket')
-export class TicketController {}
+@Controller('tickets')
+export class TicketController {
+    constructor(private ticketService: TicketService) {}
+
+    @Post('submit')
+    async createTicket(@Body() createTicketDto: CreateTicketDto): Promise<TicketDocument> {
+        return this.ticketService.createTicket(createTicketDto)
+    }
+
+    @Put(':id')
+    updateTicketStatus(@Param('id') id: string, @Body('status') status: string): Promise<TicketDocument> {
+        return this.ticketService.updateTicketStatus(id, status)
+    }
+
+    @Get('status')
+    async getTicketsByStatus(@Body() status: string): Promise<TicketDocument[]>{
+        return this.ticketService.getTicketsByStatus(status)
+    }
+
+    @Get()
+    async getAllTickets(): Promise<TicketDocument[]> {
+        return this.ticketService.getAllTickets()
+    }
+}
