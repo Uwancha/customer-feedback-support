76ca46423d6ab690a82f2ef523124f7bd8d6a15f
Uwancha <sito2alemu@gmail.com>
2024-06-29 22:47:33 +0300

    feat: Create service for feedback

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { FeedbackRepository } from './feedback.repository';
+import { FeedbackDocument } from './schemas/feedback.schema';
+import { CreateFeedbackDto } from './dto/create-feedback-dto';
 
 @Injectable()
-export class FeedbackService {}
+export class FeedbackService {
+    constructor(private feedbackRepository: FeedbackRepository) {}
+
+    async create(feedback: CreateFeedbackDto): Promise<FeedbackDocument> {
+        return this.feedbackRepository.create(feedback)
+    }
+
+    async getAllFeedbacks(): Promise<FeedbackDocument[]> {
+        return this.feedbackRepository.findAll()
+    }
+}
