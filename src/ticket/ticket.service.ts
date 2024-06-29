99684ada8c8d5069109f82ce68449f7b47ab3814
Uwancha <sito2alemu@gmail.com>
2024-06-30 01:07:34 +0300

    feat: Create service for ticket

--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { TicketRepository } from './ticket.repository';
+import { CreateTicketDto } from './dto/create-ticket-dto';
+import { TicketDocument } from './schemas/ticket.schema';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+    constructor(private ticketRepository: TicketRepository) {}
+
+    async createTicket(createTicketDto: CreateTicketDto): Promise<TicketDocument> {
+        return this.ticketRepository.create(createTicketDto)
+    }
+
+    async updateTicketStatus(id: string, status: string): Promise<TicketDocument> {
+        return this.ticketRepository.updateStatus(id, status)
+    }
+
+    async getTicketsByStatus(status: string): Promise<TicketDocument[]>{
+        return this.ticketRepository.findByStatus(status)
+    }
+
+    async getAllTickets(): Promise<TicketDocument[]> {
+        return this.ticketRepository.findAll()
+    }
+}
