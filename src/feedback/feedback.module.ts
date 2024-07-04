cccbe592d414237c069dabf62a3a8f1aefbb9c44
Uwancha <sito2alemu@gmail.com>
2024-07-05 01:15:28 +0300

    feat: Secure routes. use guards to protect certain routes

--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -4,10 +4,11 @@ import { FeedbackService } from './feedback.service';
 import { FeedbackRepository } from './feedback.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FeedBackSchema, Feedback } from './schemas/feedback.schema';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: Feedback.name, schema: FeedBackSchema}])],
   controllers: [FeedbackController],
-  providers: [FeedbackService, FeedbackRepository]
+  providers: [FeedbackService, FeedbackRepository, JwtService]
 })
 export class FeedbackModule {}
--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -4,10 +4,11 @@ import { TicketService } from './ticket.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Ticket, TicketSchema } from './schemas/ticket.schema';
 import { TicketRepository } from './ticket.repository';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: Ticket.name, schema: TicketSchema}])],
   controllers: [TicketController],
-  providers: [TicketService, TicketRepository]
+  providers: [TicketService, TicketRepository, JwtService]
 })
 export class TicketModule {}
